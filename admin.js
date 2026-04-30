a99fd9ecfd615ceedd7d2a3486025dd7f3f40d9d
Nahid Anowar <166890716+nahid6296@users.noreply.github.com>
2026-04-30 09:10:10 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBIO5fWjk3kz42ZYpkv4pPms5iHJ1wLe8I",
+    authDomain: "travel-qr-site-a2ea0.firebaseapp.com",
+    projectId: "travel-qr-site-a2ea0",
+    storageBucket: "travel-qr-site-a2ea0.firebasestorage.app",
+    messagingSenderId: "919120082507",
+    appId: "1:919120082507:web:26339f9f9a67a1af8fbfce"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
